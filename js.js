84516c60440ce32617934eef73ce062b71095b01
wolfgray <5565486@mail.ru>
2023-03-07 16:04:11 +0300

    add switcher actions

--- js.js
+++ js.js
@@ -47,12 +47,14 @@ const swiper = new Swiper('.swiper', {
   // },
 });
 
-let openMenu = document.querySelector('.header__select-menu');
-let modal = document.querySelector('.modal-container')
-let body = document.querySelector('body');
-let menuItems = document.querySelectorAll('.modal__menu-item')
+const openMenu = document.querySelector('.header__select-menu');
+const modal = document.querySelector('.modal-container')
+const body = document.querySelector('body');
+const menuItems = document.querySelectorAll('.modal__menu-item')
+const switchers = document.querySelectorAll('.switcher__label')
+const  closeBtn = document.querySelector('.close-btn')
+const switcherItems = document.querySelectorAll('.table-col')
 
-let closeBtn = document.querySelector('.close-btn')
 openMenu.addEventListener('click', ()=> {
   modal.classList.add('modal-container__open')
   body.style.overflow='hidden'
@@ -66,45 +68,21 @@ for (let el of menuItems) {
   el.addEventListener('click', () => {
     modal.classList.remove('modal-container__open')
     body.style.overflow='visible'
-  
   })
 }
-// let dFix = document.querySelector('.d-fix');
-
-// let cardItems = document.querySelectorAll('.modal-block');
-// let selectMenu = document.querySelectorAll('.modal-list');
-
-// for (let listElem of selectMenu) {
-//   listElem.onclick = function () {
-//     body.classList.toggle('d-fix');
-
-//     showAll.classList.toggle('rotate-burger');
-//     for (let elem of cardItems) {
-//       elem.classList.toggle('modal-block');
-//       elem.classList.toggle('show');
-//     }
-//   };  
-// }
-
-// showAll.onclick = function () {
-//   body.classList.toggle('d-fix');
-//   showAll.classList.toggle('rotate-burger');
-//   for (let elem of cardItems) {
-//     elem.classList.toggle('modal-block');
-//     elem.classList.toggle('show');
-
-//   }
-// };
-
-// let card = document.querySelector('.scheme-card');
-
-// let cardsFront = card.querySelector('.front');
-// let cardsBack = card.querySelector('.back');
-
-// card.addEventListener( 'click', () => {
-//  cardsFront.classList.toggle('scheme-card-rotate-front');
-//  cardsBack.classList.toggle('scheme-card-rotate-back');
-// }
-// )
-
-
+console.log(switchers);
+for (let el of switchers) {
+  el.addEventListener('click', () => {
+    for (let el of switchers) {
+      el.classList.remove('switcher__label--checked')  
+    }
+   el.classList.add('switcher__label--checked')
+    for (let elem of switcherItems){
+    elem.style.visibility='hidden'
+    elem.style.height=0
+    elem.id == "solo" && el.id == 'solo-switcher' ? (elem.style.visibility='visible',  elem.style.height='auto' ):null
+    elem.id == "team" && el.id == 'team-switcher' ? (elem.style.visibility='visible', elem.style.height='auto'):null
+    }
+    
+  })
+}
