f724c4cbd6ec28bf09317474898ae3aada35fc79
wolfgray <5565486@mail.ru>
2023-03-08 19:18:37 +0300

    fix switcher flag

--- js.js
+++ js.js
@@ -1,19 +1,19 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper(".swiper", {
   // Optional parameters
-  direction: 'horizontal',
+  direction: "horizontal",
   loop: false,
 
   // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
     clickable: true,
     dynamicBullets: true,
   },
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 
   // And if we need scrollbar
@@ -47,48 +47,49 @@ const swiper = new Swiper('.swiper', {
   // },
 });
 
-const openMenu = document.querySelector('.header__select-menu');
-const modal = document.querySelector('.modal-container')
-const body = document.querySelector('body');
-const menuItems = document.querySelectorAll('.modal__menu-item')
-const switchers = document.querySelectorAll('.switcher__label')
-const  closeBtn = document.querySelector('.close-btn')
-const switcherItems = document.querySelectorAll('.table-col')
-const main = document.querySelector('.main')
+const openMenu = document.querySelector(".header__select-menu");
+const modal = document.querySelector(".modal-container");
+const body = document.querySelector("body");
+const menuItems = document.querySelectorAll(".modal__menu-item");
+const switchers = document.querySelectorAll(".switcher__label");
+const closeBtn = document.querySelector(".close-btn");
+const switcherItems = document.querySelectorAll(".table-col");
+const main = document.querySelector(".main");
 
-openMenu.addEventListener('click', ()=> {
-  modal.classList.add('modal-container__open')
-const screenHeight = window.innerHeight
-modal.style.height = `${screenHeight}px`
-
-
-})
-closeBtn.addEventListener('click', () => {
-  modal.classList.remove('modal-container__open')
-
-})
+openMenu.addEventListener("click", () => {
+  modal.classList.add("modal-container__open");
+  const screenHeight = window.innerHeight;
+  modal.style.height = `${screenHeight}px`;
+});
+closeBtn.addEventListener("click", () => {
+  modal.classList.remove("modal-container__open");
+});
 for (let el of menuItems) {
-  el.addEventListener('click', () => {
-    modal.classList.remove('modal-container__open')
-    body.style.overflow='visible'
-  })
+  el.addEventListener("click", () => {
+    modal.classList.remove("modal-container__open");
+    body.style.overflow = "visible";
+  });
 }
 for (let el of switchers) {
-  el.addEventListener('click', () => {
-    el.classList
+  el.addEventListener("click", () => {
     for (let el of switchers) {
-      el.classList.add('switcher__label--non-checked') 
-      el.classList.remove('switcher__label--checked')
+      el.classList.remove("switcher__label--checked-solo");
+      el.classList.remove("switcher__label--checked-team");
 
     }
-    el.classList.remove('switcher__label--non-checked')  
-    el.classList.add('switcher__label--checked')
-    for (let elem of switcherItems){
-    elem.style.visibility='hidden'
-    elem.style.height=0
-    elem.id == "solo" && el.id == 'solo-switcher' ? (elem.style.visibility='visible',  elem.style.height='auto' ):null
-    elem.id == "team" && el.id == 'team-switcher' ? (elem.style.visibility='visible', elem.style.height='auto'):null
+    // el.classList.add("switcher__label--checked");
+
+    el.id == "team-switcher" ? el.classList.add("switcher__label--checked-team") : el.classList.add("switcher__label--checked-solo");
+
+    for (let elem of switcherItems) {
+      elem.style.visibility = "hidden";
+      elem.style.height = 0;
+      elem.id == "solo" && el.id == "solo-switcher"
+        ? ((elem.style.visibility = "visible"), (elem.style.height = "auto"))
+        : null;
+      elem.id == "team" && el.id == "team-switcher"
+        ? ((elem.style.visibility = "visible"), (elem.style.height = "auto"))
+        : null;
     }
-    
-  })
+  });
 }
